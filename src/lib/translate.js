60231995d8875a189dfccef52d8f75232e381741
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2017-11-23 23:51:01 +0100

    Warn if template not found (because missing from config)

--- src/lib/translate.js
+++ src/lib/translate.js
@@ -8,5 +8,13 @@ const makeTemplateFunction = tmpl =>
 export default config => {
   const templateFunctions = mapValues(config.text, makeTemplateFunction);
 
-  return (key, data) => templateFunctions[key](data);
+  return (key, data) => {
+    const tmpl = templateFunctions[key];
+    if (tmpl == null) {
+      console.warn(`template ${key} not found`);
+      return '';
+    } else {
+      return tmpl(data);
+    }
+  };
 };
